a215166a0b52ce60b4c37ea7b0169dc23b9b38bf
cristianguzmxn <31904999+cristianguzmxn@users.noreply.github.com>
2017-09-26 17:43:24 -0400

    fortune-teller

---
+++ js/fortune.js
@@ -0,0 +1,40 @@
+
+
+var fortune=new Array()
+
+
+
+fortune[0]="Which is better: holding onto anger or holding onto bad memories? The opposition between powerful Mars and passive Neptune is asking. When you quit thinking of the past and angry thoughts, they deteriorate. Goodbye, old thoughts! Tonight, beware of expecting something too soon if you are looking for love."
+
+
+
+fortune[1]='Give conflicts more thought and insight, because they could be the universe’s way of pushing you to make changes. Tonight, enter fantasyland with supersensitive feelings when the moon squares both Mars and Neptune. Fasten your seat belt! Life is about as realistic as the imagination can make it.'
+
+
+
+fortune[2]='It’s a busy week, astrologically, as lovely Venus moves into practical Virgo, encouraging you to see to your well-being. A new moon in this same sign suggests that it might be time to get back on track with a healthier diet or exercise routine. As the bright sun enters congenial Libra, you might also find that relationships require more positive attention. It would help to take it easy over the weekend, though, as you may feel less energetic than usual.'
+
+
+
+fortune[3]=' You may have a strange tingly feeling up your spine when you consider the alternatives on offer today. The energy of the day may mean that you suddenly find yourself the center of attention. As this is your natural place, anyway, it may not be a surprise. But what is surprising is the number of potential partners who are willing to totally adore you. How sickeningly purrrfect!'
+
+
+fortune[4]="This week concerns your inner feelings. Your emotions may be a bit tumultuous, especially when it comes to work. Rather than give in to a job you don't like, use this time to think about what you would rather do or what would have more meaning. If you can hold your head high, you will get more done and earn more."
+
+
+
+fortune[5]='Make a firm decision to control anger or worry today. Any tendency to blame or speak critically about others will work against your best interests. Negative expectations and fears are not true. Remain calm, even in a situation where you have to take a stand against a difficult person.'
+
+
+
+fortune[6]="You're likely to regret it if you say the wrong thing to someone you live or work near. On top of hurt feelings, you may have to explain yourself to people caught on the sidelines. Let a spirit of fun color your decisions today. You have nothing to prove."
+
+
+
+
+
+
+
+var whichfortune=Math.floor(Math.random()*(fortune.length))
+
+document.write(fortune[whichfortune])
